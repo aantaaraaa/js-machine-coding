95127d24bd70c53417afe3e26fbf4f348570ee02
Antara Chanda <160458895+meantaraa@users.noreply.github.com>
2025-11-11 12:21:52 +0530

    star rating component

---
+++ 3.star-rating/main.js
@@ -0,0 +1,23 @@
+const stars = document.querySelectorAll(".star");
+const selectedRatingValueText = document.querySelector(".selected-rating-value");
+let currentTotalSelectedStars = 0;
+
+function paint(n) {
+  stars.forEach((s, i) => {
+    if (i < n) s.classList.add('filled');
+    else s.classList.remove('filled');
+  });
+}
+
+stars.forEach((star, idx) => {
+  const rating = idx + 1;
+  star.dataset.rating = rating;
+
+  star.addEventListener('mouseover', () => paint(rating));
+  star.addEventListener('mouseout', () => paint(currentTotalSelectedStars));
+  star.addEventListener('click', () => {
+    currentTotalSelectedStars = rating;
+    paint(currentTotalSelectedStars);
+    if (selectedRatingValueText) selectedRatingValueText.textContent = currentTotalSelectedStars;
+  });
+});
